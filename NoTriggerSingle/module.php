cda5f5021090f8e66190f0ce4e79bc16d5c9064d
Nall-chan <micha@nall-chan.net>
2016-11-14 17:32:07 +0100

    no message

--- NoTriggerSingle/module.php
+++ NoTriggerSingle/module.php
@@ -44,38 +44,8 @@ class NoTriggerSingle extends IPSModule {
 
 ################## DUMMYS / WOARKAROUNDS - protected
 
-    protected function HasActiveParent()
-    {
-        IPS_LogMessage(__CLASS__, __FUNCTION__); //          
-        $instance = IPS_GetInstance($this->InstanceID);
-        if ($instance['ConnectionID'] > 0)
-        {
-            $parent = IPS_GetInstance($instance['ConnectionID']);
-            if ($parent['InstanceStatus'] == IS_ACTIVE)
-                return true;
-        }
-        return false;
-    }
 
-    protected function SetStatus($data) {
-        IPS_LogMessage(__CLASS__, __FUNCTION__); //           
-    }
 
-    protected function RegisterTimer($data, $cata) {
-        IPS_LogMessage(__CLASS__, __FUNCTION__); //           
-    }
-
-    protected function SetTimerInterval($data, $cata) {
-        IPS_LogMessage(__CLASS__, __FUNCTION__); //           
-    }
-
-    protected function LogMessage($data, $cata) {
-        
-    }
-
-    protected function SetSummary($data) {
-        IPS_LogMessage(__CLASS__, __FUNCTION__ . "Data:" . $data); //                   
-    }
 
 }
 
